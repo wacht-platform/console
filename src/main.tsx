2ddf526f82cac329e5a564b06cb3361b3df91123
Saurav Singh <snipextt@gmail.com>
2025-05-27 09:50:51 +0530

    bump lib version

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { DeploymentProvider } from "@snipextt/wacht";
 import "./index.css";
 import App from "./App.tsx";
-import { DeploymentProvider } from "@snipextt/wacht";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
