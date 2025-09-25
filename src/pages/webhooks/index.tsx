ec8a2b00fb4dcbd2d721dd72f54a37513643680c
Saurav Singh <snipextt@gmail.com>
2025-09-25 13:02:58 +0530

    remove unused imports

--- src/pages/webhooks/index.tsx
+++ src/pages/webhooks/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useParams, useNavigate } from "react-router";
+import { useParams } from "react-router";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Heading } from "@/components/ui/heading";
