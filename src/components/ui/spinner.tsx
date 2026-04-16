fce837fc075f5d1fe56648f8c1c5489898e74e24
Saurav Singh <snipextt@gmail.com>
2026-04-17 00:52:16 +0530

    Update spinner.tsx

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,10 +1,16 @@
-import { cn } from "@/lib/utils"
-import { RiLoaderLine } from "@remixicon/react"
+import { cn } from "@/lib/utils";
+import { RiLoaderLine } from "@remixicon/react";
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
-  return (
-    <RiLoaderLine role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
-  )
+    return (
+        <RiLoaderLine
+            role="status"
+            aria-label="Loading"
+            className={cn("size-4 animate-spin", className)}
+            children={undefined as any}
+            {...props}
+        />
+    );
 }
 
-export { Spinner }
+export { Spinner };
